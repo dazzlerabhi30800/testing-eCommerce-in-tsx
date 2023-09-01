41aad34372a2edf56378cf8b19109053ce23a323
= <abhisingh30800@gmail.com>
2023-09-01 18:27:41 +0530

    remove nextApiRequest

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Data from './data.json';
 
 
@@ -13,6 +13,6 @@ type Data = {
     discount: number,
 }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse<Array<Data>>) {
+export async function GET(req: NextRequest, res: NextApiResponse<Array<Data>>) {
     return NextResponse.json(Data);
 }
